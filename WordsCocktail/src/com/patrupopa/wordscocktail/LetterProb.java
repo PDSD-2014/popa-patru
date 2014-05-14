d82cacdf31fc2c599a1f08a93cced3a60907361f
diannne <pdiana.maria91@gmail.com>
2014-05-14 17:41:22 +0300

    Added class LetterProb referenced in Game.java

---
+++ WordsCocktail/src/com/patrupopa/wordscocktail/LetterProb.java
@@ -0,0 +1,38 @@
+package com.patrupopa.wordscocktail;
+
+import java.util.ArrayList;
+
+public class LetterProb {
+	private String _letter;
+	private ArrayList<Integer> _probabilities;
+	public LetterProb(String letter)
+	{
+		_letter = letter;
+		_probabilities = new ArrayList<Integer>();
+	}
+
+	public void addProbability(Integer prob)
+	{
+		_probabilities.add(prob);
+	}
+	public void addProbabilities(ArrayList<Integer> probs)
+	{
+		_probabilities.addAll(probs);
+	}
+	
+	public int getTop()
+	{
+		return _probabilities.get(0);
+	}
+	
+	public int removeTop()
+	{
+		return _probabilities.remove(0);
+	}
+	public String getLetter()
+	{
+		return _letter;
+	}
+	
+	
+}
